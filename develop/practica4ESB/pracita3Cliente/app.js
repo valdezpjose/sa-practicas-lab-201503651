1302980177d60c99be39cd04cb0040cb128ba07d
Jose Fernando Valdez Perez <jvaldez@bdgsa.net>
2021-03-07 21:50:18 -0600

    push a develop

---
+++ develop/practica4ESB/pracita3Cliente/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const axios = require('axios');
+const { response } = require('express');
+const { connect } = require('http2');
+
+const {json, urlencoded} = express;
+
+const app = express();
+
+const port = process.env.PORT  || 8081;
+
+const corsOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+
+
+app.get('/cliente/solicitarPedido', async  (req, res) =>{
+    var respuesta;
+    await axios.get('http://localhost:8080/restaurante/recibirPedido').then(resp => {
+         respuesta = resp.data;
+    });
+    res.send(respuesta);
+})
+
+app.get('/cliente/verificarPedidoRestaurante', async  (req, res) =>{
+    var respuesta;
+    await axios.get('http://localhost:8080/restaurante/estadoPedido').then(resp => {
+         respuesta = resp.data;
+    });
+    res.send(respuesta);
+})
+
+app.get('/cliente/verificarPedidoRepartidor', async  (req, res) =>{
+    var respuesta;
+    await axios.get('http://localhost:8090/repartidor/estadoPedido').then(resp => {
+         respuesta = resp.data;
+    });
+    res.send(respuesta);
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`puerto ${port}`);
+})
+
